79cb429c92169a79af2c5a8cb9e769678e931900
Shravya <shravyashettigar8@gmail.com>
2026-06-25 13:30:39 +0530

    Initial commit

---
+++ login.py
@@ -0,0 +1,37 @@
+from flask import Flask, Blueprint, render_template, request, redirect, url_for, session 
+import sqlite3 
+from database import get_connection 
+ 
+ 
+login_bp = Blueprint('login', __name__ ) 
+logout_bp = Blueprint('logout', __name__ ) 
+ 
+@login_bp.route('/', methods=['GET', 'POST']) 
+def login():
+    if 'login' in session: 
+        return redirect(url_for('dashboard.dashboard')) 
+    if request.method == 'POST': 
+        username = request.form['username'] 
+        password = request.form['password'] 
+        
+        conn = get_connection() 
+        conn.row_factory = sqlite3.Row 
+        cursor = conn.cursor() 
+ 
+ 
+        user = cursor.execute("SELECT * FROM login WHERE username=? AND password = ?", (username, password)).fetchone()
+
+        conn.close() 
+ 
+        if user: 
+            session['login'] = username 
+            return redirect(url_for('dashboard.dashboard'))
+        else: 
+            return render_template("login.html", error="Invalid Username/Password")
+    return render_template("login.html") 
+    
+# Logout Route 
+@login_bp.route('/logout') 
+def logout(): 
+    session.clear() 
+    return redirect(url_for('login.login')) 
